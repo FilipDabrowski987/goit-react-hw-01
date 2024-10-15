fca73396695dfd43a5d0fc8f00208c6db2e043ad
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-10-15 16:54:03 +0200

    add_props_Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import Profile from './Profile/Profile.jsx';
+import userData from './Profile/userData.json';
 
 export default function App() {
   return (
     <div>
-      <h1>Profiles</h1>
-
-      <Profile />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
     </div>
   );
 }
